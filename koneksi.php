41bb24466a7e1b4dc3a0ba715633d8a3b000eec3
Razziqdhav <razziqdhaf@gmail.com>
2026-04-17 17:30:41 +0700

    Add Docker setup for PHP and MySQL

--- koneksi.php
+++ koneksi.php
@@ -1,4 +1,14 @@
 <?php
-$conn = mysqli_connect("localhost","root","","laporsampah");
+$dbHost = getenv('DB_HOST') ?: 'localhost';
+$dbUser = getenv('DB_USER') ?: 'root';
+$dbPass = getenv('DB_PASS') ?: '';
+$dbName = getenv('DB_NAME') ?: 'laporsampah';
+
+$conn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
+
+if (!$conn) {
+    die('Connection failed: ' . mysqli_connect_error());
+}
+
 session_start();
 ?>
